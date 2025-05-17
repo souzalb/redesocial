747f05c59ca6944f18c106f760b98aab60144e05
sn1082087 <lincoln.souza@docente.senai.br>
2025-05-16 21:24:34 -0300

    feat: add homepage and perfil pages

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def homepage():
-    return 'Rede Social de Fotos - Tela Inicial'
+    return render_template("homepage.html")
 
-@app.route("/perfil")
-def perfil():
-    return 'Rede Social de Fotos - Perfil'
+@app.route("/perfil/<usuario>")
+def perfil(usuario):
+    return render_template("perfil.html", usuario=usuario)
 
 if __name__ == '__main__':
     app.run(debug=True)
