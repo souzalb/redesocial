46bb44431accf2fba2a73bfbed8bfe670463a3c2
sn1082087 <lincoln.souza@docente.senai.br>
2025-05-20 21:03:52 -0300

    chore: database configuration and modeling

--- criar_banco.py
+++ criar_banco.py
@@ -1,4 +1,6 @@
 from rede import database, app
+from rede.models import User, Photo
 
 with app.app_context():
     database.create_all()
+
--- rede/models.py
+++ rede/models.py
@@ -1,8 +1,16 @@
 from rede import database
+from datetime import datetime, timezone
 
 class User(database.Model):
-    pass
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String(20), unique=True, nullable=False)
+    email = database.Column(database.String(120), unique=True, nullable=False)
+    password = database.Column(database.String(60), nullable=False)
+    photos = database.relationship('Photo', backref='user', lazy=True)
 
 class Photo(database.Model):
-    pass
+    id = database.Column(database.Integer, primary_key=True)
+    file_name = database.Column(database.String(255), default="default.png")
+    upload_date = database.Column(database.DateTime, nullable=False, default=datetime.now(timezone.utc))
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
 
