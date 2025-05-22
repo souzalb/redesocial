0a26abdc7f27b2e2b7146a7d413219bc907ba8da
sn1082087 <lincoln.souza@docente.senai.br>
2025-05-21 21:13:35 -0300

    feat: login and security system setup

--- rede/__init__.py
+++ rede/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SECRET_KEY'] = 'f89aa21b6aeb311a3c889df7e1ec5f0c'
 
 database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'homepage'
 
 from rede import routes
 
--- rede/models.py
+++ rede/models.py
@@ -1,7 +1,12 @@
-from rede import database
+from rede import database, login_manager
 from datetime import datetime, timezone
+from flask_login import UserMixin
 
-class User(database.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(database.Model, UserMixin):
     id = database.Column(database.Integer, primary_key=True)
     username = database.Column(database.String(20), unique=True, nullable=False)
     email = database.Column(database.String(120), unique=True, nullable=False)
