30ba1be978ce6549a9f68729b6743bad3f129f71
sn1082087 <lincoln.souza@docente.senai.br>
2025-05-21 21:44:59 -0300

    feat: add class LoginForm and RegisterFrom

--- rede/forms.py
+++ rede/forms.py
@@ -1,4 +1,23 @@
-import secrets
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from rede.models import User
 
-print(secrets.token_hex(16))
+class LoginForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class RegisterForm(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    username = StringField('Nome de usuário', validators=[DataRequired(), Length(2, 20)])
+    password = PasswordField('Senha', validators=[DataRequired(), Length(6, 60)])
+    confirm_password = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Criar Conta')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            return ValidationError("E-mail já cadastrado. Por favor, use outro e-mail ou faça login para continuar.")
+        return None
 
