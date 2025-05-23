fd190b50587cb8f6eb77ce8deb1b7b3c04882294
sn1082087 <lincoln.souza@docente.senai.br>
2025-05-22 21:03:15 -0300

    feat: add loginForm and registerForm on pages

--- rede/routes.py
+++ rede/routes.py
@@ -1,13 +1,22 @@
 from flask import render_template, url_for
 from rede import app
 from flask_login import login_required
+from rede.forms import LoginForm, RegisterForm
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def homepage():
-    return render_template("homepage.html")
+    loginform = LoginForm()
+    return render_template("homepage.html", form=loginform)
+
+@app.route("/criarconta", methods=["GET", "POST"])
+def criarconta():
+    registerform = RegisterForm()
+    return render_template("criarconta.html", form=registerform)
 
 @app.route("/perfil/<usuario>")
 @login_required
 def perfil(usuario):
     return render_template("perfil.html", usuario=usuario)
+
+
 
