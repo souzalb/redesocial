76d83b257702d5bd947b44da7601abbf67a2a312
sn1082087 <lincoln.souza@docente.senai.br>
2025-06-03 18:38:39 -0300

    feat: add login and create account functionality

--- rede/routes.py
+++ rede/routes.py
@@ -1,16 +1,35 @@
-from flask import render_template, url_for
-from rede import app
-from flask_login import login_required
+from flask import render_template, url_for, redirect
+from rede import app, database, bcrypt
+from rede.models import User, Photo
+from flask_login import login_required, login_user, logout_user
 from rede.forms import LoginForm, RegisterForm
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
     loginform = LoginForm()
+    if loginform.validate_on_submit():
+        user = User.query.filter_by(email=loginform.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, loginform.password.data):
+            login_user(user)
+            return redirect(url_for("perfil", usuario=user.username))
     return render_template("homepage.html", form=loginform)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("homepage"))
 
 @app.route("/criarconta", methods=["GET", "POST"])
 def criarconta():
     registerform = RegisterForm()
+    if registerform.validate_on_submit():
+        password = bcrypt.generate_password_hash(registerform.password.data)
+        user = User(username=registerform.username.data, password=password, email=registerform.email.data)
+        database.session.add(user)
+        database.session.commit()
+        login_user(user, remember=True)
+        return redirect(url_for("perfil", usuario=user.username))
     return render_template("criarconta.html", form=registerform)
 
 @app.route("/perfil/<usuario>")
