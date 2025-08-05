9b223a16c11cc15007d935f9373cb325f710bf8b
sn1082087 <lincoln.souza@docente.senai.br>
2025-08-04 21:50:51 -0300

    chore: add enviroments variables of database

--- rede/__init__.py
+++ rede/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SECRET_KEY'] = 'f89aa21b6aeb311a3c889df7e1ec5f0c'
 app.config['UPLOAD_FOLDER'] = 'static/posts_photos'
 
